d7c379eb757498dadcaba63e42019972b0131dd5
denny <mail2dny@gmail.com>
2026-09-11 16:34:11 +0700

    Update build_blocklists.py

--- scripts/build_blocklists.py
+++ scripts/build_blocklists.py
@@ -86,6 +86,7 @@ LISTS: dict[str, tuple[str, ...]] = {
     "webserver": ("webserver",),
     "compact1": ("etblock","feodo","toxic","hijack","dshield7","abuseipdb7"),
     "compact2": ("etblock","feodo","hijack","dshield7","abuseipdb7"),
+    "compact3": ("etblock","feodo","dshield7","abuseipdb7"),
     "combined1": ("etblock","feodo","toxic","webserver","hijack","dshield7","abuseipdb7"),
     "combined2": ("etblock","feodo","toxic","webserver","hijack","dshield30","abuseipdb30","strongips"),
     "complete": ("etblock","feodo","toxic","webserver","hijack","dshield30","abuseipdb30","strongips","level2","level4","botnet"),
