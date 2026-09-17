8820770084eab91b53260dac5faccef87b4c2862
denny <mail2dny@gmail.com>
2026-09-17 15:27:35 +0700

    Update build_blocklists_v7.py

--- scripts/build_blocklists_v7.py
+++ scripts/build_blocklists_v7.py
@@ -37,7 +37,7 @@ SOURCES: dict[str, str] = {
     "threatfox": ("https://raw.githubusercontent.com/elliotwutingfeng/ThreatFox-IOC-IPs/main/ips.txt"),
     "tif": ("https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/ips/tif.txt"),
     "threatview": ("https://threatview.io/Downloads/High-Confidence-CobaltStrike-C2%20-Feeds.txt"),
-    #"alienvault": ("http://reputation.alienvault.com/reputation.data"),#ERROR
+    "alienvault": ("http://reputation.alienvault.com/reputation.data"),
     #"blocklistde": ("https://lists.blocklist.de/lists/all.txt"), #level2
     #"toxic": ("https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/stopforumspam_toxic.netset"), #webserver
     #"hijack": ("https://raw.githubusercontent.com/kraloveckey/ipsets-blocklist/main/iblocklist_hijacked.netset"), #level4
@@ -82,7 +82,7 @@ LISTS: dict[str, tuple[str, ...]] = {
     "compact": ("base7","ipsum7","baseip"),
     "combined": ("base3","ipsum3","baseip"),
     "combined4server": ("base3","threatfox","level4","abuser","botnet","abuseipdb","threatview"),
-    "complete": ("combined","threatfox","level2","level3","tif"),
+    "complete": ("combined","threatfox","level2","level3","tif","alientvault"),
 }
 
 EXPORTS: dict[str, bool] = {
